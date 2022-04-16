72c5df52a9b2cc66f104efa0039854d8ca06e654
zarkaiya <98179944+zarkaiya@users.noreply.github.com>
2022-04-16 15:25:37 +0530

    8jan automation github practice

--- src/pack1/GitHubClass1.java
+++ src/pack1/GitHubClass1.java
@@ -4,7 +4,9 @@ public class GitHubClass1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		System.out.println("Github simple project1");
+		
 	}
 
 }
---
+++ src/pack2/GitHubClass2.java
@@ -0,0 +1,11 @@
+package pack2;
+
+public class GitHubClass2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Crete new github class2");
+	}
+
+}
